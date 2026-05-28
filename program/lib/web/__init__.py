68d23793170421994c43d036eadc6811c468f797
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2026-05-28 20:28:00 -0300

    Update site

--- program/lib/web/__init__.py
+++ program/lib/web/__init__.py
@@ -112,7 +112,11 @@ class _LogShareRequestHandler(BaseHTTPRequestHandler):
 	def _send_cors_headers(self):
 		self.send_header('Access-Control-Allow-Origin', '*')
 		self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
-		self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+		self.send_header(
+			'Access-Control-Allow-Headers',
+			'Content-Type, Access-Control-Request-Private-Network',
+		)
+		self.send_header('Access-Control-Allow-Private-Network', 'true')
 		self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
 		self.send_header('Pragma', 'no-cache')
 
