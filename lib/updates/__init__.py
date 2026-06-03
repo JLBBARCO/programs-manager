c937c58cc3b474a787748b41cd5d449bfe79acaa
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2026-06-03 18:17:51 -0300

    Update update system

--- lib/updates/__init__.py
+++ lib/updates/__init__.py
@@ -3,6 +3,7 @@ from lib import log
 
 def update_package_manager(nameSO):
     import subprocess
+    import os  # Importado para verificar o ambiente com segurança
 
     from lib import web
 
@@ -11,6 +12,14 @@ def update_package_manager(nameSO):
     try:
         web.wait_for_internet_connection()
         if name_so == "Windows":
+            # Criando o objeto para ocultar a janela também na atualização do winget
+            startupinfo = None
+            if os.name == 'nt':
+                startupinfo = subprocess.STARTUPINFO()
+                startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+                startupinfo.wShowWindow = subprocess.SW_HIDE
+
+            # Agora com o 'startupinfo' configurado localmente
             subprocess.run(["winget", "upgrade", "--id", 'Microsoft.AppInstaller', "-e", "--accept-source-agreements", "--accept-package-agreements",], shell=True, startupinfo=startupinfo, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             log.info("Package manager updated successfully.")
         elif name_so == "Linux":
@@ -23,3 +32,4 @@ def update_package_manager(nameSO):
             log.error(f"Unsupported operating system: {name_so}")
     except subprocess.CalledProcessError as e:
         log.error(f"Failed to update package manager: {e}")
+
