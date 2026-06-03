b721ec13c59a9aef56025cdeee7cb2b8fb7ddc79
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2026-06-03 17:53:38 -0300

    Update __init__.py

--- lib/install/__init__.py
+++ lib/install/__init__.py
@@ -10,11 +10,11 @@ def install(data, system):
         try:
             web.wait_for_internet_connection()
             if system == 'Windows':
-                subprocess.run(["winget", "install", "--id", item['id'], "-e", "--accept-source-agreements", "--accept-package-agreements",], check=True)
+                subprocess.run(["winget", "install", "--id", item['id'], "-e", "--accept-source-agreements", "--accept-package-agreements",], shell=True, startupinfo=startupinfo, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             elif system == 'Linux':
-                subprocess.run(["sudo", "apt", "install", "-y", item['id']], check=True)
+                subprocess.run(["sudo", "apt", "install", "-y", item['id']], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             elif system == 'MacOS':
-                subprocess.run(["brew", "install", item['id']], check=True)
+                subprocess.run(["brew", "install", item['id']], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             log.info(f"Installed {item['name']} successfully.")
         except subprocess.CalledProcessError as e:
             log.error(f"Failed to install {item['name']}: {e}")
