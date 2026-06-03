f698ac6916a4af7765c940c01c13a05e05658586
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2026-06-03 17:55:35 -0300

    Update __init__.py

--- lib/uninstall/__init__.py
+++ lib/uninstall/__init__.py
@@ -10,11 +10,11 @@ def uninstall(data, system):
         try:
             web.wait_for_internet_connection()
             if system == 'Windows':
-                subprocess.run(["winget", "uninstall", "--id", item['id']], check=True)
+                subprocess.run(["winget", "uninstall", "--id", item['id']], shell=True, startupinfo=startupinfo, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             elif system == 'Linux':
-                subprocess.run(["sudo", "apt", "remove", "-y", item['id']], check=True)
+                subprocess.run(["sudo", "apt", "remove", "-y", item['id']], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             elif system == 'MacOS':
-                subprocess.run(["brew", "uninstall", item['id']], check=True)
+                subprocess.run(["brew", "uninstall", item['id']], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             log.info(f"Uninstalled {item['name']} successfully.")
         except subprocess.CalledProcessError as e:
             log.error(f"Failed to uninstall {item['name']}: {e}")
